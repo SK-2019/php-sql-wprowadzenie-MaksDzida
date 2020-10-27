ec194654bb23860a53895c2924259c0505a808f2
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-10-27 13:16:45 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+    $conn= new mysqli("127.0.0.1","root","","nauka1");
+    $result=$conn->query('SELECT * FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        
+            while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                        echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                        echo("</tr>");
+            }            
+        echo("</table>");
+
+        $conn= new mysqli("127.0.0.1","root","","nauka1");
+    $result=$conn->query('SELECT * FROM pracownicy where imie like "%a"');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        
+            while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                        echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                        echo("</tr>");
+            }            
+        echo("</table>");
+
+    $conn= new mysqli("127.0.0.1","root","","nauka1");
+    $result=$conn->query('SELECT * FROM pracownicy where imie not like "%a"');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                        echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                        echo("</tr>"); 
+            }
+        echo("</table>");
+
+        $conn= new mysqli("127.0.0.1","root","","nauka1");
+        $result=$conn->query('SELECT * FROM pracownicy where zarobki>10');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
+
+        $conn= new mysqli("127.0.0.1","root","","nauka1");
+        $result=$conn->query('SELECT * FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
+
+        $conn= new mysqli("127.0.0.1","root","","nauka1");
+        $result=$conn->query('SELECT * FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+            echo("</tr>");
+        }
+            echo("</table>");
+
+            $conn= new mysqli("127.0.0.1","root","","nauka1");
+            $result=$conn->query('SELECT * FROM pracownicy');
+            echo("<table border=1>");
+            echo("<th>id</th>");
+            echo("<th>imie</th>");
+            echo("<th>dzial</th>");
+            echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+           echo("</tr>"); 
+            }
+            echo("</table>");
+
+
+
+
+        
+            
+
+            
+            
+?>
