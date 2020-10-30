be0ad81ae430438d2c922ff04914c104aab70f73
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-10-30 13:02:17 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "remotemysql.com" ;
+$username =  "2CMNLoipEx" ;
+$password = "MVjnZFDUhb" ;
+$dbname = "2CMNLoipEx"
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  ?>
