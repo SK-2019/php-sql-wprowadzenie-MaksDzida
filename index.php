a72b05cc46f9af3c9fb20c174a3cac0d1e2410ab
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-04 15:00:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
 </head>
 <body>
     
-    
+ <div class="link"><strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-MaksDzida">Github</a></strong></div>   
 <?php
 
 
 
 
     echo("<h1>Maks Dzida</h1>");
-    <div class="link"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-MaksDzida">Github</a></div>
+    
    require_once('connect.php');
 echo("<li>ZADANIE 1 SELECT * FROM pracownicy</li>");
     $result=$conn->query('SELECT * FROM pracownicy');
