16b2542af3eb9823f34c30efaacbaf026b8e64d0
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-06 12:47:35 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -28,8 +28,26 @@ echo("<li> SELECT * FROM pracownicy</li>");
 
         echo("</table>");
    
-   echo("<li> SELECT * FROM pracownicy </li>");
+   echo("<li> SELECT * FROM pracownicy where dzial=2 </li>");
     $result=$conn->query("SELECT * from pracownicy where dzial=2");
+
+       echo("<table border=1>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dział</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data urodzenia</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
+   
+     echo("<li> SELECT * FROM pracownicy where dzial=2 </li>");
+    $result=$conn->query("SELECT * from pracownicy where (dzial=2 or dzial=3");
 
        echo("<table border=1>");
         echo("<th>ID</th>");
