d5f422d8a4d809f11bc909b3eb8277116273141a
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-06 13:50:30 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -129,6 +129,25 @@ echo("<li>SELECT * FROM pracownicy</li>");
             }
 
         echo("</table>");
+	
+	echo("<li>SELECT * FROM pracownicy, organizacja WHERE dzial = id_org and imie like %a order by imie asc </li>");
+    
+  $result = $conn->query('SELECT * FROM pracownicy, organizacja WHERE dzial = id_org and imie like "%a" order by imie asc');
+        echo("<table border=1>");
+	echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
     
     ?>
     </body>
