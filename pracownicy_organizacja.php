9023fc8a34d212da57c7942736329ec1a82cd0b3
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-06 13:33:50 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -113,7 +113,7 @@ echo("<li>SELECT * FROM pracownicy</li>");
 	
 	echo("<li>SELECT * FROM pracownicy, organizacja WHERE dzial = id_org order by imie desc</li>");
     
-  $result = $conn->query('SELECT * FROM pracownicy, organizacja WHERE dzial = id_org and dzial=3 order by asc');
+  $result = $conn->query('SELECT * FROM pracownicy, organizacja WHERE dzial = id_org and dzial=3 order by');
         echo("<table border=1>");
 	echo("<th>ID</th>");
         echo("<th>Imie</th>");
