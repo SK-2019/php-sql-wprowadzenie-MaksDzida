b4ee4f33a2336aee0c8369a9204e1f10d138b93a
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-06 09:08:29 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -8,24 +8,25 @@
 <a href="https://github.com/SK-2019/php-sql-wprowadzenie-MaksDzida"> link do mojego githuba </a>
    <a class="nav_link" href="funkcjeAgregujace.php">Funkcje Agregujace</a>
 <?php
-    require_once('conn.php');
-    $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');
-        echo("<hr />");
-        echo("<h3>Tabela Pracowników</h3>");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
+    require_once('connect.php');
+echo("<li> SELECT * FROM pracownicy</li>");
+    $result=$conn->query("SELECT id_pracownicy, imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial=id_org");
+
+       echo("<table border=1>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dział</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data urodzenia</th>");
+            while($row=$result->fetch_assoc()){ 
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+
                 echo("</tr>");
             }
+
         echo("</table>");
-        
+     
 ?>
     </body>
 </html>
