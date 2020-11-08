19f6c31bc08f5fde058f660d134b069a7cc37e5f
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-08 14:42:52 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -8,6 +8,7 @@
 <strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-MaksDzida">Github</a></strong>
 <div class="nav">
     <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+     <a class="nav_link" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
     <a class="nav_link" href="index.php">Strona</a>
 </div>
  <h1>Maks Dzida</h1>   
