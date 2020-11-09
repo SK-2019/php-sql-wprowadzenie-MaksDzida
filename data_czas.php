5de21df85c3c26235334842abf0de028a15b3eb4
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-10 00:08:06 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -17,7 +17,7 @@
 <?php
    require_once('connect.php');
      
-        echo("<h2>ZADANIE 1: SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org</h2>");
+        echo("<li> SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org</li>");
        $result = $conn->query('SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org');
            echo("<table border=1>");
            echo("<th>Id</th>");
@@ -33,6 +33,26 @@
                    echo("</tr>");
                }
     echo("</table>");
+    
+    echo("<li>SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org AND nazwa_dzial='serwis'</li>");
+           $result = $conn->query('SELECT *, YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy, organizacja WHERE dzial=id_org AND nazwa_dzial="serwis"');
+               echo("<table border=1>");
+               echo("<th>Id</th>");
+               echo("<th>Imie</th>");
+               echo("<th>Dział</th>");
+               echo("<th>Nazwa_Działu</th>");
+               echo("<th>Zarobki</th>");
+               echo("<th>Data_urodzenia</th>");
+               echo("<th>Wiek</th>");
+                   while($row=$result->fetch_assoc()){ 
+                       echo("<tr>");
+                       echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["wiek"]."</td>"); 
+                       echo("</tr>");
+                   }
+       
+               echo("</table>");
+    
+    
   
 ?>
 </body>
