88272763944e9c76251fe50e61df79207eae1e84
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-18 14:46:17 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -3,6 +3,17 @@
 echo($_POST['Imie']);
 echo($_POST['Nazwisko']);
 
+require_once('conn.php');
+  $sql = "INSERT INTO pracownicy (Imie, Nazwisko)
+  VALUES ('John', 'Doe')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+
 
 
 ?>
