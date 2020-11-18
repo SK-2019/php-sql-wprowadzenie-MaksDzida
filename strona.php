5d9991f62659c8a7aac5766fdf720ec4589655d1
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-18 15:26:55 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -4,7 +4,7 @@ echo("<li>Imie: ".$_POST['Imie']."</li>");
 echo("<li>Nazwisko: ".$_POST['Nazwisko']."</li>");
 
 require_once("connect.php");
-  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['Imie']."',1,25,'01-01-1975')";
+  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['Imie']."',1,25,'1999-01-01')";
   
   
   if ($conn->query($sql) === TRUE) {
