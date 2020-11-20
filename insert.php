556c718fc7d225254248cae991dc067af11df7bc
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-20 14:53:00 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,8 @@
 <?php
+echo("<li>Imie: ".$_POST['imie']."</li>");
+echo("<li>Dzial: ".$_POST['dzial']."</li>");
+echo("<li>Zarobki: ".$_POST['zarobki']."</li>");
+echo("<li>Data urodzenia: ".$_POST['data_ur']."</li>");
 
   require_once("connect.php");
 
