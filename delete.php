abad5ca3f69b7406b9c920de147e37f5e551013c
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-20 14:59:25 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,9 +1,10 @@
 <?php
-echo("<li>ID: ".$_POST['id']."</li>");
+
+  echo("<li>ID: ".$_POST['id']."</li>");
 
   require_once("connect.php");
 
-  $sql = "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."';
+  $sql = "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
   
   
   if ($conn->query($sql) === TRUE) {
