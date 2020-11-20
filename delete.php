da5dcdaa6e3a4089b87684a57a883ba07c3dd638
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-20 14:57:58 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,15 @@
+<?php
+echo("<li>ID: ".$_POST['id']."</li>");
 
+  require_once("connect.php");
+
+  $sql = "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."';
+  
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+?>
