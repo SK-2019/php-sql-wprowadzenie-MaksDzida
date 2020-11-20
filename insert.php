3d6d5d302f29cfded8e288ee29bec59d7540cdd0
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-11-20 14:52:20 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,14 @@
+<?php
+
+  require_once("connect.php");
+
+  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."','".$_POST['dzial']."','".$_POST['zarobki']."' ,'".$_POST['data_ur']."')";
+  
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+?>
