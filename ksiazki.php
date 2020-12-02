a5a6eea318c9c54b097294f5c4a6103a71469eb9
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-12-02 15:12:59 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -1,1 +1,22 @@
+<?php
+  
+ require_once('connect.php');
+echo("<h1>Książki i autorzy:</h1>");
+$sql = 'SELECT * FROM biblTytul';
+echo("<h2>Tytuły:</h2>");
+echo("<li>".$sql);
 
+ $result = $conn->query($sql);
+        echo("<table>");
+        echo("<th>ID</th>");
+        echo("<th>Tytuł</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
+
+?>
