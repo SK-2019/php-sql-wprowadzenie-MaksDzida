7e1714db9b019288bb1f2da3f467670e6e5cf3be
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-12-02 15:17:55 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -1,4 +1,22 @@
-<?php
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-MaksDzida">Github</a></strong>
+<div class="nav">
+    <a class="nav_link" href="index.php">Strona</a>
+    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+     <a class="nav_link" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+  <a class="nav_link" href="funkcAgregujace.php">Funkcje Agregujące</a>
+   <a class="nav_link" href="data_czas.php">Data i Czas</a>
+</div>
+ <h1>Maks Dzida</h1>   
+  
+  <php
   
  require_once('connect.php');
 echo("<h1>Książki i autorzy:</h1>");
