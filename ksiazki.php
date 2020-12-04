670a4c60758e42fc1597abff06322655d1cf21b3
MaksDzida <73532546+MaksDzida@users.noreply.github.com>
2020-12-04 08:28:16 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -20,7 +20,7 @@
   <?php
   
  require_once('connect.php');
-echo("<h1>Biblioteka:</h1>");
+echo("<h1>Książki i ich autorzy:</h1>");
 $sql = 'SELECT * FROM biblTytul';
 echo("<h2>Tytuły:</h2>");
 echo("<li>".$sql);
@@ -32,6 +32,24 @@ echo("<li>".$sql);
             while($row=$result->fetch_assoc()){ 
                 echo("<tr>");
                     echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
+    
+    require_once('connect.php');
+$sql = 'SELECT * FROM biblAutor';
+echo("<h2>Autorzy:</h2>");
+echo("<li>".$sql);
+
+ $result = $conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>ID</th>");
+        echo("<th>Autorzy</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id"]."</td><td>".$row["autor"]."</td>"); 
 
                 echo("</tr>");
             }
