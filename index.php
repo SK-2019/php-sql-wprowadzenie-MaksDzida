2924678e2418cb55659282aa0511705448ca2b7a
MaksDzida <maks.dzida@gmail.com>
2021-02-02 13:34:41 +0100

    c

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <title> Maks Dzida 2Ti gr2 </title>
 <link rel="stylesheet" href="assets/style1.css">
-< link  rel = " icon " href = " https://cdn.galleries.smcloud.net/t/galleries/gf-adXv-uHGX-gbwR_harambe-664x0-nocrop.jpg " >
+
 </head>
 <body>
 <!-- sprawdzenie polaczenia -->
