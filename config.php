ca27ccb19c9afb72b6f19f9e4d25fbc7e8a0e690
MaksDzida <maks.dzida@gmail.com>
2021-04-17 16:39:58 +0200

    s

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
 $_SERVER['servername'] = "remotemysql.com";
-$_SERVER['password'] = "GhRaQReEzu";
 $_SERVER['username'] = "Z50XCiuXyG";
+$_SERVER['password'] = "GhRaQReEzu";
 $_SERVER['dbname'] = "Z50XCiuXyG";
-
 $_SERVER['pass'] = "maks_local"
 
 
