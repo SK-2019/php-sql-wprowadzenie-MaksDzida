2cc77a208ac7f8682cc7be191dabd71f1ccc8241
MaksDzida <maks.dzida@gmail.com>
2021-04-20 16:59:54 +0200

    s

--- pracownicy/pracownicy_organizacja.php
+++ pracownicy/pracownicy_organizacja.php
@@ -12,7 +12,7 @@
   <strong><a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-MaksDzida">Github</a></strong>
    <a class="nav_link" href="/index.php">Strona</a>
     <a class="nav_link" href="/pracownicy/pracownicy.php">Pracownicy - wstęp</a>
-     <a class="nav_link" href="/pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+     <a class="nav_link" href="../pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a>
   <a class="nav_link" href="/pracownicy/funkcAgregujace.php">Funkcje Agregujące</a>
    <a class="nav_link" href="/pracownicy/data_czas.php">Data i Czas</a>
      <a class="nav_link" href="/ćwiczenia/formularz.html">Formularz</a>
