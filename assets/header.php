8bbf51de0aeb6b797b1b573cd1e3f63a1803ccea
MaksDzida <maks.dzida@gmail.com>
2021-04-20 13:15:10 +0200

    a

---
+++ assets/header.php
@@ -0,0 +1,3 @@
+<title> Maks Dzida 2Ti gr2 </title>
+<link rel="stylesheet" href="assets/style1.css">
+<link  rel = "icon" href = "https://cdn.galleries.smcloud.net/t/galleries/gf-adXv-uHGX-gbwR_harambe-664x0-nocrop.jpg">
