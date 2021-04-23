e6f0a286d3d5aa016697b2d4fb3437537bbf90ea
MaksDzida <maks.dzida@gmail.com>
2021-04-23 15:37:36 +0200

    S

--- index.php
+++ index.php
@@ -9,23 +9,11 @@
 <!-- sprawdzenie polaczenia -->
 <div class='container'>
 
-<div class='name'><h1>Maks Dzida</h1></div>
-<div class="nav">
-<strong><a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-MaksDzida">Github</a></strong>
-<a class="nav_link" href="/index.php">Strona</a>
- <a class="nav_link" href="/pracownicy/pracownicy.php">Pracownicy - wstęp</a>
-  <a class="nav_link" href="/praacownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a>
-<a class="nav_link" href="/pracownicy/funkcAgregujace.php">Funkcje Agregujące</a>
-<a class="nav_link" href="/pracownicy/data_czas.php">Data i Czas</a>
-  <a class="nav_link" href="/ćwiczenia/formularz.html">Formularz</a>
- <a class="nav_link" href="/pracownicy/daneDoBazy.php">Dodaj Dane</a>
- <a class="nav_link" href="/ćwiczenia/function.php">Funkcja</a>
-   <a class="nav_link" href="/biblioteka/ksiazki.php">Biblioteka</a>
-   <a class="nav_link" href="/biblioteka/wieledowielu.php">Wiele do wielu</a>
-   <a class="nav_link" href="/flexbox/flexbox.html">Próbny flexbox</a>
-</div>  
- <div class='phpbox'>
 
+<?php
+      include("assets/header.php");
+      include("assets/menu.php");
+      ?>
 
 <?php
 
