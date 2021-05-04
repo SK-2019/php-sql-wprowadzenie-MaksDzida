beb38d1a643f4ff3e9bdbf4cb2d78bb37820553e
MaksDzida <maks.dzida@gmail.com>
2021-05-04 15:52:38 +0200

    UP

--- relacje/del2wieledowielu2.php
+++ relacje/del2wieledowielu2.php
@@ -1,6 +1,6 @@
 <?php
 require("../assets/connect.php");
-$sql = "DELETE FROM klasa where id='".$_POST['id']."'";
+$sql = "DELETE FROM klasa where id_klasa='".$_POST['id']."'";
 $conn->query($sql);
 header("location:wieledowielu2.php");
 ?>
